b96199278050dc43d8e4ee8bcb931363930bbde5
Vitali Klein <barss1805@mail.ru>
2022-01-29 22:30:57 +0300

    fix a mistake in sentences/admin.py

--- into_english_and_back/sentences/admin.py
+++ into_english_and_back/sentences/admin.py
@@ -2,6 +2,6 @@ from django.contrib import admin
 
 from .models import Sentence, Time, Success
 
-admin.site.register("Sentence")
-admin.site.register("Time")
-admin.site.register("Success")
+admin.site.register(Sentence)
+admin.site.register(Time)
+admin.site.register(Success)
