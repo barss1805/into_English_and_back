4d03a10d23ed9be1dbd0594c566b3f02d6e32389
Vitali Klein <barss1805@mail.ru>
2022-01-29 22:25:12 +0300

    register in admin panel models Sentence, Time, Succes

--- into_english_and_back/sentences/admin.py
+++ into_english_and_back/sentences/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Sentence, Time, Success
+
+admin.site.register("Sentence")
+admin.site.register("Time")
+admin.site.register("Success")
