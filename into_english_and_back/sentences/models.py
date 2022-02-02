0f10ed80dd38cf4f95b2398ff53b1d9a1975464d
Vitali Klein <barss1805@mail.ru>
2022-02-02 16:35:57 +0300

    delete field 'slug' from 'Sentence' class

--- into_english_and_back/sentences/models.py
+++ into_english_and_back/sentences/models.py
@@ -3,7 +3,6 @@ from django.db import models
 
 class Sentence(models.Model):
     time = models.ForeignKey('Time', on_delete=models.PROTECT)
-    slug = models.SlugField(max_length=255, unique=True, verbose_name="URL")
     eng_lang = models.CharField(max_length=1000)
     rus_lang = models.CharField(max_length=1000)
     created_at = models.DateTimeField(auto_now_add=True)
